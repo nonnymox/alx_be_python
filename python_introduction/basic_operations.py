ab613dd6dbe6b8b2cfa2381526f8d323b4c73c57
nonnymox <knonxho@gmail.com>
2024-09-10 17:16:20 -0700

    Updated Py file

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+addition = number1 + number2
+difference = number2 - number1
+product = number1 * number2
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Subtraction of {number1} and {number2} is {difference}")
+print(f"Multiplication of {number1} and {number2} is {product}")
