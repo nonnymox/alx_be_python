05f86dd815197a2a8cb9a8119796e24c70061883
Emmanuel Chukwunonso Ighavbota <emmanuelighavbota@gmail.com>
2024-09-20 14:39:04 +0100

    added alternative with while loop

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,3 +1,12 @@
 user_input = int(input("Enter the size of the pattern: "))
-for i in range(0, user_input):
-    print("*" * user_input)
+i = 0
+
+while i < user_input:
+    for j in range(user_input):
+        print("*", end="")
+    print()
+    i += 1
+
+# user_input = int(input("Enter the size of the pattern: "))
+# for i in range(0, user_input):
+#     print("*" * user_input)
