ea16be632f7e4d94817f7ff90b7e85cc7caba30d
Emmanuel Chukwunonso Ighavbota <emmanuelighavbota@gmail.com>
2024-09-28 09:24:03 +0100

    Did arithmetic operations.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,28 +1,11 @@
-task = input("Enter your task: ")
-priority = input("Priority (high/medium/low): ")
-time_bound = input("Is it time-bound? (yes/no): ")
+day = input("What is today?").lower()
 
-
-match priority:
-    case "high":
-        if time_bound == "yes":
-            print(f"Reminder: '{task}' is a high priority task that requires immediate attention today!")
-        else:
-            print(f"'Note: {task}' is still a high priority task that requires some attention soon!")
-    case "medium":
-        if time_bound == "yes":
-            print(f"Reminder: '{task}' is a medium priority task that requires immediate attention today!")
-        else:
-            print(f"Note: '{task}' is still a medium priority task that requires some attention. Consider completing it "
-                  f"when you have free time.!")
-    case "low":
-        if time_bound == "yes":
-            print(f"Reminder: '{task}' is a low priority task that requires immediate attention today!")
-        else:
-            print(f"Note: '{task}' is still a low priority task. Consider completing it when you have free time.")
-    case _:
-        print(f"Unknown priority level for '{task}'. Please enter 'low', 'medium', or 'high'.")
-
-
-
+match day:
+    case "monday":
+        print("Shoot a movie")
+    case "tuesday":
+        print("Read a book")
+    case "wednesday":
+        print("Go to church")
+    case "we"
 
