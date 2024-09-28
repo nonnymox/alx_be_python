997947708499effb68015077c265eb24ba90d40b
Emmanuel Chukwunonso Ighavbota <emmanuelighavbota@gmail.com>
2024-09-28 11:34:58 +0100

    Created shopping list manager

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,35 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            # Prompt for and add an item
+            add_item = input("What would you like to add?")
+            shopping_list.append(add_item)
+        elif choice == '2':
+            # Prompt for and remove an item
+            remove_item = input("What will you like to remove?")
+            shopping_list.remove(remove_item)
+        elif choice == '3':
+            # Display the shopping list
+            return shopping_list
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+
+if __name__ == "__main__":
+    main()
+
