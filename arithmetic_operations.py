36963b94a6696cb61408631300737de50e3c851e
Emmanuel Chukwunonso Ighavbota <emmanuelighavbota@gmail.com>
2024-09-28 10:24:44 +0100

    Did arithmetic operations.py

--- arithmetic_operations.py
+++ arithmetic_operations.py
@@ -5,7 +5,10 @@ def perform_operation(num1, num2, operation):
         return num1 - num2
     elif operation == "multiply":
         return num1 * num2
-    elif operation == "divide"
-        return num1 / num2
+    elif operation == "divide":
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Cannot divide by zero."
     else:
         return "That is not valid!"
