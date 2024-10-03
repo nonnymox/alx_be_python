21295968abc7bc144e44b3e049fb3c343cc03646
Emmanuel Chukwunonso Ighavbota <emmanuelighavbota@gmail.com>
2024-10-03 17:01:29 +0100

    Used OOP to add Bank Accout class and methods

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,20 @@
+class BankAccount:
+    def __init__(self, account_balance=0):
+        # Initialize the account balance
+        self.account_balance = account_balance
+
+    def deposit(self, amount):
+        # Add the deposit amount to the account balance
+        self.account_balance += amount
+
+    def withdraw(self, amount):
+        # Check if there's enough balance for the withdrawal
+        if self.account_balance >= amount:
+            self.account_balance -= amount
+            return True
+        else:
+            return False
+
+    def display_balance(self):
+        # Return the current balance
+        return f"Your balance is {self.account_balance}"
