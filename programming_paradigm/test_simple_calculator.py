8c69aa6d699723420c9db943052fe493ba879f3e
Emmanuel Chukwunonso Ighavbota <emmanuelighavbota@gmail.com>
2024-10-05 16:21:14 +0100

    Wrote unittests for simple calculator

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,6 +1,7 @@
 import unittest
 from simple_calculator import SimpleCalculator
 
+
 class TestSimpleCalculator(unittest.TestCase):
 
     def setUp(self):
@@ -12,7 +13,18 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
         # Add more assertions to thoroughly test the add method.
+
     def test_subtraction(self):
         """Test the subtraction method."""
-        self.assertEqual(self.calc.subtract(5,6), -1)
+        self.assertEqual(self.calc.subtract(5, 6), -1)
         self.assertEqual(self.calc.subtract(-1, 6), -7)
+
+    def test_multiply(self):
+        """Test the multiplication method"""
+        self.assertEqual(self.calc.multiply(2,3), 6)
+        self.assertEqual(self.calc.multiply(0, 8), 0)
+
+    def test_division(self):
+        """Test the division method"""
+        self.assertEqual(self.calc.divide(6, 0), None)
+        self.assertEqual(self.calc.divide(3, 2), 1.5)
