486b4f9556354d28bfaec16727edb1e636a06785
Emmanuel Chukwunonso Ighavbota <emmanuelighavbota@gmail.com>
2024-10-05 10:49:43 +0100

    Modified the display to return a float

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -17,4 +17,4 @@ class BankAccount:
 
     def display_balance(self):
         # Return the current balance
-        return f"Current Balance: ${self.account_balance:.2f}"
+        print(f"Current Balance: ${self.account_balance:.2f}")
