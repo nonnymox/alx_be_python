c8695d6d42dc8ca98721e206584d628d51e69c3e
Emmanuel Chukwunonso Ighavbota <emmanuelighavbota@gmail.com>
2024-10-11 18:51:10 +0100

    Did the Second Exercise on inheritance

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,17 +3,26 @@ class Book:
         self.title = title
         self.author = author
 
+    def __str__(self):
+        print(f"Book: {self.title} by {self.author}")
+
 
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = file_size
 
+    def __str__(self):
+        print(f"EBook: {self.title} by {self.author}, File Size: {self.file_size}")
+
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = page_count
+
+    def __str__(self):
+        print(f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}")
 
 
 class Library:
